4bd37dcc4719439d1c3e3999e42d11682b46753b
Tom Houlé <tom@tomhoule.com>
2019-06-08 00:58:43 +0200

    sketch 03 - rotating square

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,25 @@ fn sketch_02(app: &nannou::App, frame: nannou::Frame) -> nannou::Frame {
     frame
 }
 
+/// Rotating square
+fn sketch_03(app: &nannou::App, frame: nannou::Frame) -> nannou::Frame {
+    let draw = app.draw();
+    let window_rect = app.window_rect();
+    draw.background().color(nannou::color::BLACK);
+
+    draw.rect()
+        .color(nannou::color::LIGHT_ORANGE)
+        .width(window_rect.w() / 3.0)
+        .height(window_rect.w() / 3.0)
+        .rotate((app.duration.since_start.as_millis() as f32 / 500.0) / 3.14)
+        .x(0.0)
+        .y(0.0);
+
+    draw.to_frame(app, &frame).unwrap();
+
+    frame
+}
+
 fn main() {
-    nannou::sketch(sketch_02);
+    nannou::sketch(sketch_03);
 }
