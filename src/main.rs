2f5c050dc978e3de73d105bb1d0f476df575e135
Tom Houlé <tom@tomhoule.com>
2019-06-10 10:31:44 +0200

    Hexagons attempt

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,85 @@ fn sketch_03(app: &nannou::App, frame: nannou::Frame) -> nannou::Frame {
     frame
 }
 
+struct App01Model {
+    points: Vec<(u32, u32)>,
+}
+
+impl App01Model {
+    /// In pixels.
+    const SIDE_LENGTH: u32 = 14;
+
+    fn new(app: &App) -> App01Model {
+        let (w, h) = (1000, 600);
+        let mut points = Vec::with_capacity(
+            (w / Self::SIDE_LENGTH + 1) as usize * (h / Self::SIDE_LENGTH + 1) as usize,
+        );
+
+        let window = app
+            .new_window()
+            .with_dimensions(w, h)
+            .view(Self::view)
+            .build()
+            .unwrap();
+
+        let mut cursor: (u32, u32) = (0, 0);
+        let bottom_right: (u32, u32) = app.main_window().inner_size_pixels();
+
+        let finished_drawing = |cursor: (u32, u32)| cursor.1 > bottom_right.1;
+
+        let mut row = 0;
+
+        while !finished_drawing(cursor) {
+            points.push(cursor);
+
+            if cursor.0 > bottom_right.0 {
+                // Next line
+                row += 1;
+                cursor = (
+                    if row % 3 == 0 { Self::SIDE_LENGTH } else { 0 },
+                    cursor.1 + Self::SIDE_LENGTH,
+                );
+            } else {
+                let narrow = row % 3 == 0;
+                // Next point
+                cursor = (cursor.0 + Self::SIDE_LENGTH * 2, cursor.1);
+            }
+        }
+
+        App01Model { points }
+    }
+
+    fn view(app: &App, model: &Self, frame: Frame) -> Frame {
+        frame.clear(nannou::color::WHITE);
+
+        let window_rect = app.window_rect();
+        let top_left = window_rect.top_left();
+
+        let draw = app.draw();
+
+        for point in &model.points {
+            draw.ellipse()
+                .radius(4.0)
+                .color(nannou::color::RED)
+                .x(top_left.x + point.0 as f32)
+                .y(top_left.y - point.1 as f32)
+                // .w(20.0)
+                // .h(20.0)
+                .finish()
+                .unwrap();
+        }
+
+        draw.to_frame(app, &frame).unwrap();
+
+        frame
+    }
+}
+
+fn app_01() {
+    nannou::app(App01Model::new).run();
+}
+
 fn main() {
-    nannou::sketch(sketch_03);
+    // nannou::sketch(sketch_03);
+    app_01()
 }
